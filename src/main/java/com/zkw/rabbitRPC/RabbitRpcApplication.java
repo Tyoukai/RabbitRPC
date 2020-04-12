720fc75ff81c4ec2a2b4bbe5c6799f0a9cc539f0
Tyoukai <zhang_k_w@163.com>
2020-04-12 17:04:00 +0800

    init

---
+++ src/main/java/com/zkw/rabbitRPC/RabbitRpcApplication.java
@@ -0,0 +1,13 @@
+package com.zkw.rabbitRPC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitRpcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RabbitRpcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zkw/rabbitRPC/RabbitRpcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zkw.rabbitRPC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitRpcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
